3cae310a342f7c84b0b31c456950c48a3bcad397
Nathan Wang <nxwang@ucsc.edu>
2022-07-07 19:53:35 -0700

    Tilemap collision inverse

--- Assets/Maps/Tilesets/forest-tiles.tsx
+++ Assets/Maps/Tilesets/forest-tiles.tsx
@@ -1,47 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="forest-wide" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+<tileset version="1.9" tiledversion="1.9.0" name="forest-wide" tilewidth="32" tileheight="32" tilecount="64" columns="8">
  <image source="images/gras-tiles-32px.png" width="256" height="256"/>
- <tile id="2">
-  <objectgroup draworder="index" id="2">
-   <object id="3" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="5">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="31">
-    <polygon points="0,0 29,-1 31,-31 0,-31"/>
-   </object>
-  </objectgroup>
- </tile>
  <tile id="6">
   <properties>
    <property name="Wall" type="bool" value="true"/>
   </properties>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="1" y="0">
-    <polygon points="0,0 1,30 31,31 31,0"/>
-   </object>
-  </objectgroup>
  </tile>
  <tile id="9">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0.0625">
-    <polygon points="0,0 0,31.75 32,0.0625"/>
+   <object id="3" x="30.25" y="7.25">
+    <polygon points="1.75,-7.25 1.625,24.625 -30.25,24.75"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="10" probability="0.5"/>
+ <tile id="10" probability="0.5">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="11">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0.125" y="0.125">
-    <polygon points="0,0 31.6875,31.6875 31.75,-0.1875"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="13">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1">
-    <polygon points="0,0 30,1 31,31 0,31"/>
+    <polygon points="-0.125,-0.125 31.875,31.875 -0.125,31.875"/>
    </object>
   </objectgroup>
  </tile>
@@ -49,23 +29,20 @@
   <properties>
    <property name="Wall" type="bool" value="true"/>
   </properties>
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="1" y="32">
-    <polygon points="0,0 1,-30 31,-31 31,0"/>
-   </object>
-  </objectgroup>
  </tile>
- <tile id="16">
+ <tile id="17" probability="0.5">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="17" probability="0.5"/>
- <tile id="18" probability="5"/>
- <tile id="19" probability="0.5"/>
- <tile id="20">
+ <tile id="18" probability="5">
   <objectgroup draworder="index" id="2">
-   <object id="3" x="0" y="0" width="32" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" probability="0.5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="22" probability="10">
@@ -76,31 +53,25 @@
  <tile id="25">
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.375" y="-0.0625">
-    <polygon points="0,0 32.125,32.25 0.3125,32.125"/>
+    <polygon points="0.375,0.0625 32.375,32.0625 32.375,0.0625"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="26" probability="0.5"/>
- <tile id="27">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0.25" y="31.8125">
-    <polygon points="0,0 31.5625,-31.5625 31.8125,0.375"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="33">
+ <tile id="26" probability="0.5">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="34">
+ <tile id="27">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="0" width="32" height="32"/>
+   <object id="1" x="0.25" y="31.8125">
+    <polygon points="-0.25,0.1875 31.75,-31.8125 -0.25,-31.8125"/>
+   </object>
   </objectgroup>
  </tile>
- <tile id="35">
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="0" y="0" width="32" height="32"/>
+ <tile id="56">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="30" height="30"/>
   </objectgroup>
  </tile>
 </tileset>
