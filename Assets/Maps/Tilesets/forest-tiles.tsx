56cf5068f7181f75a2a96298feb1825d2cb68ad0
Nathan Wang <nxwang@ucsc.edu>
2022-07-07 19:56:05 -0700

    Full square marker

--- Assets/Maps/Tilesets/forest-tiles.tsx
+++ Assets/Maps/Tilesets/forest-tiles.tsx
@@ -70,8 +70,8 @@
   </objectgroup>
  </tile>
  <tile id="56">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="1" y="1" width="30" height="30"/>
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
 </tileset>
