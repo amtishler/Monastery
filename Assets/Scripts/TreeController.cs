5b2bdf28ca139c8726052fe639a0a00b325a21ee
Andy Cepollina <andycepi@live.com>
2022-07-12 01:07:47 -0700

    update tree sprites and add particles to tutorial level

---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerGO;
+    [SerializeField]
+    Vector3 treeMidpoint;
+
+    SpriteRenderer sR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sR = GetComponent<SpriteRenderer>();
+        playerGO = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerGO.transform.position.y >= transform.position.y + treeMidpoint.y) {
+            sR.sortingLayerName = "Foreground Trees";
+        } else
+        {
+            sR.sortingLayerName = "Background Trees";
+        }
+    }
+}
