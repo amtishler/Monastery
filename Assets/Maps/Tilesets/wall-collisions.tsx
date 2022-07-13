6741a478249cb640a31a6bdbff7d262dc80d1dac
Andy Cepollina <andycepi@live.com>
2022-07-12 18:12:03 -0700

    reworking wall collisions

---
+++ Assets/Maps/Tilesets/wall-collisions.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="wall-collisions" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="images/wall-collisions.png" width="96" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,33"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,32 32,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="32">
+    <polygon points="0,0 32,-32 32,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 17,16 32,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
