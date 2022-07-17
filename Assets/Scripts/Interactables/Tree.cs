ebb557adc11839fa90df97ab64116aa1645c1ddd
HCEspino <harrison.espino@gmail.com>
2022-07-16 23:06:52 -0700

    The Tree drops health

--- Assets/Scripts/Interactables/Tree.cs
+++ Assets/Scripts/Interactables/Tree.cs
@@ -8,21 +8,47 @@ public class Tree : InteractableObject
     public GameObject drop;
     public int maxdrops;
     public bool dropused;
+    private bool hit;
+    public GameObject childdrop;
+
+    public float dropy = -2;
+    public float dropx = 2; 
 
     private Animator animator;
+    private int numdropsused;
 
     void Start()
     {
+        dropused = false;
+        numdropsused = 0;
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        animator.SetBool("Hit", hit);
+        if(hit)
+        {
+            hit = false;
+        }
+        if(this.transform.childCount == 0 && numdropsused < maxdrops)
+        {
+            dropused = false;
+        }
     }
 
     public override void OnHit(Vector3 dir, float mag)
     {
-        animator.Play("Tree1-Hit");
+        hit = true;
+        if(!dropused && drop != null)
+        {
+            numdropsused++;
+            dropused = true;
+            childdrop = Instantiate(drop) as GameObject;
+            childdrop.transform.parent = this.transform;
+
+            Vector3 childpos = new Vector3(dropx, dropy, 0);
+            childdrop.transform.position = this.transform.position + childpos;
+        }
     }
 }
