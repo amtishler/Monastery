2dde9cd1b2df18e7d7c389ed15abaea32d5e02b5
Andy Cepollina <andycepi@live.com>
2022-07-22 01:46:33 -0700

    shadow shader updates

---
+++ Assets/Scripts/Art Scripts/ShadowDepthController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowDepthController : MonoBehaviour
+{
+    SpriteRenderer parentSR;
+    SpriteRenderer sR;
+    int parentSROrder = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sR = GetComponent<SpriteRenderer>();
+        parentSR = transform.parent.GetComponent<SpriteRenderer>();
+        parentSROrder = parentSR.sortingOrder;
+        Debug.Log(parentSR.sortingLayerName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sR.sortingLayerName = parentSR.sortingLayerName;
+        sR.sortingOrder = parentSROrder - 1;
+    }
+}
