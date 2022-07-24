ed881d6f29fe7f6cfebe9da54461563e291c027c
Andy Cepollina <andycepi@live.com>
2022-07-24 05:00:05 -0700

    lots of visual changes
    -background and clouds, animated
    - drop shadows on the walls
    - update shaders + materials
    - reorganize art assets

---
+++ Assets/Scripts/Art Scripts/DropShadow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DropShadow : MonoBehaviour
+{
+    public Material shadowMat;
+    public Vector3 offset;
+    public GameObject original;
+    public GameObject grid;
+
+    GameObject shadowGO;
+
+    void Start()
+    {
+        shadowGO = GameObject.Instantiate(original);
+        shadowGO.transform.SetParent(grid.transform, false);
+        shadowGO.transform.position = shadowGO.transform.position + offset;
+        shadowGO.GetComponent<TilemapRenderer>().material = shadowMat;
+        shadowGO.name = original.name + "_shadow";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
